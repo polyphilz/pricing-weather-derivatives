3cd0d03230f9065de5e529b713547f433f7ccc0f
Rohan Bansal <rohanarunbansal@gmail.com>
2018-08-13 17:31:17 -0400

    Cleaned up scraper and got relevant columns only

--- scrape_data.py
+++ scrape_data.py
@@ -6,7 +6,7 @@ from datetime import datetime, time, date
 import time
 
 
-def getRainfallData(station, day, month, year):
+def get_temperature_data(station, day, month, year):
     """
     Function to return a data frame of minute-level weather data for a single Wunderground PWS station.
 
@@ -33,9 +33,10 @@ def getRainfallData(station, day, month, year):
 
     # Convert to pandas dataframe (fails if issues with weather station)
     try:
-        dataframe = pd.read_csv(io.StringIO(data), index_col=False)
-        dataframe["station"] = station
-    except Exception as e:
+        dataframe = pd.read_csv(
+            io.StringIO(data), usecols=["Time", "TemperatureF"], index_col=False
+        )
+    except:
         print(
             "Issue with date: {}-{}-{} for station {}".format(day, month, year, station)
         )
@@ -69,11 +70,11 @@ def main():
             done = False
             while done == False:
                 try:
-                    weather_data = getRainfallData(
+                    weather_data = get_temperature_data(
                         station, date.day, date.month, date.year
                     )
                     done = True
-                except ConnectionError as e:
+                except ConnectionError:
                     # May get rate limited by Wunderground.com, backoff if so.
                     print("Got connection error on {}".format(date))
                     print("Will retry in {} seconds".format(backoff_time))
@@ -81,7 +82,7 @@ def main():
             # Add each processed date to the overall data
             data[station].append(weather_data)
         # Finally combine all of the individual days and output to CSV for analysis.
-        pd.concat(data[station]).to_csv("data_weather.csv".format(station))
+        pd.concat(data[station]).to_csv("temp_data_raw.csv".format(station))
 
 
 if __name__ == "__main__":
