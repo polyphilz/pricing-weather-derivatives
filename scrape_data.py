9e95b1e00c8a47d4117d373d3e25cdf7ccc19275
Rohan Bansal <rohanarunbansal@gmail.com>
2018-08-13 17:06:08 -0400

    Hard formatted scraper script w/ Black

--- scrape_data.py
+++ scrape_data.py
@@ -5,6 +5,7 @@ from dateutil import parser, rrule
 from datetime import datetime, time, date
 import time
 
+
 def getRainfallData(station, day, month, year):
     """
     Function to return a data frame of minute-level weather data for a single Wunderground PWS station.
@@ -19,24 +20,29 @@ def getRainfallData(station, day, month, year):
         Pandas Dataframe with weather data for specified station and date.
     """
 
-    url = "http://www.wunderground.com/weatherstation/WXDailyHistory.asp?ID={station}&day={day}&month={month}&year={year}&graphspan=day&format=1"
+    url = (
+        "http://www.wunderground.com/weatherstation/WXDailyHistory.asp?ID={station}&day={day}&month={month}&year={year}&graphspan=day&format=1"
+    )
     full_url = url.format(station=station, day=day, month=month, year=year)
 
     # Request data from wunderground data
     response = requests.get(full_url)
     data = response.text
     # remove the excess <br> from the text data
-    data = data.replace('<br>', '')
+    data = data.replace("<br>", "")
 
     # Convert to pandas dataframe (fails if issues with weather station)
     try:
         dataframe = pd.read_csv(io.StringIO(data), index_col=False)
-        dataframe['station'] = station
+        dataframe["station"] = station
     except Exception as e:
-        print("Issue with date: {}-{}-{} for station {}".format(day,month,year, station))
+        print(
+            "Issue with date: {}-{}-{} for station {}".format(day, month, year, station)
+        )
         return None
 
     return dataframe
+
 
 def main():
     # Generate a list of all of the dates we want data for
@@ -63,7 +69,9 @@ def main():
             done = False
             while done == False:
                 try:
-                    weather_data = getRainfallData(station, date.day, date.month, date.year)
+                    weather_data = getRainfallData(
+                        station, date.day, date.month, date.year
+                    )
                     done = True
                 except ConnectionError as e:
                     # May get rate limited by Wunderground.com, backoff if so.
@@ -74,6 +82,7 @@ def main():
             data[station].append(weather_data)
         # Finally combine all of the individual days and output to CSV for analysis.
         pd.concat(data[station]).to_csv("data_weather.csv".format(station))
+
 
 if __name__ == "__main__":
     main()
