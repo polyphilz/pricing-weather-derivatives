417af9878a00ea7f3cffed925879d09a9e4cf721
Rohan Bansal <rohanarunbansal@gmail.com>
2018-08-13 23:44:44 -0400

    Updated model

--- avg_temperature_model.py
+++ avg_temperature_model.py
@@ -1,18 +1,29 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
-import statsmodels.tsa.arima_model as arima
+import matplotlib
 import statsmodels.api as sm
+import statsmodels.tsa.arima_model as arima
+
+def initial_plot(df):
+    plt.plot(df['mean'], color='#ff9999', linewidth=0.5)
+    plt.title('Mean Temperature in Fahrenheit from 2010-2018')
+    plt.xlabel('Time (Days)')
+    plt.ylabel('Mean Temperature (˚F)')
+    plt.savefig('initial_plot.svg', dpi=200)
+    plt.savefig('initial_plot.png', dpi=200)
+
+def stuff():
+    # df.plot(figsize=(16,8), xlim=('2010-07-30', '2011-07-30'))
+    # fig = plt.figure(figsize=(16,8))
+    # ax1 = fig.add_subplot(211)
+    # acf = sm.graphics.tsa.plot_acf(df['mean'].values, lags=30, ax=ax1)
+    # arma = arima.ARMA(df['mean'], (4,1), dates=df.index).fit()
+    pass
 
 def main():
-    df = pd.read_csv('data_weather copy.csv', index_col='Time', parse_dates=True)
+    df = pd.read_csv('temp_data_cleaned.csv', index_col='Time', parse_dates=True)
 
-    grouped.plot(figsize=(16,8), xlim=('2010-07-30', '2011-07-30'))
-    fig = plt.figure(figsize=(16,8))
-    ax1 = fig.add_subplot(211)
-    acf = sm.graphics.tsa.plot_acf(grouped['mean'].values, lags=30, ax=ax1)
-    arma = arima.ARMA(grouped['mean'], (4,1), dates=grouped.index).fit()
+    initial_plot(df)
 
 if __name__ == "__main__":
     main()
